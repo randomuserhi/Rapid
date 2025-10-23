6649c493d6d5ccb6878b6a0dc679a9a16ef8bf06
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2025-10-23 16:19:02 +0100

    chore: typescript babel

---
+++ src/ASL/Compiler/ASLBabel.config.ts
@@ -0,0 +1,33 @@
+// TODO(randomuserhi): Refine and test this...
+
+import type { TransformOptions } from "@babel/core";
+
+const config: TransformOptions = {
+    "presets": [
+        [
+            "@babel/preset-env",
+            Object.assign({}, process.env.NODE_ENV === "test"
+                ? {
+                    "loose": true,
+                    "targets": {
+                        "node": 8,
+                        "browsers": [">0.25%", "not IE"]
+                    }
+                }
+                : {
+                    "useBuiltIns": "entry",
+                    "corejs": "3.8",
+                    "targets": {
+                        "browsers": "defaults, not ie 11, not ie_mob 11"
+                    },
+                    "modules": false
+                }
+            )
+        ]
+    ],
+    "plugins": [
+        "./asl-babel-plugin"
+    ],
+};
+
+export default config;
